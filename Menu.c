24aed928c365b0475286e6e4e2cc618c1beb9b4e
PabloChristianno <pablochristianno@gmail.com>
2022-04-12 21:13:19 -0300

    Começando a implementar o menu

---
+++ Menu.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+    int op;
+    printf("Escolha uma das opcoes: \n\n");
+    printf("1 - Gerar ABP\n");
+    printf("2 - Calcular Fator de Balanceamento\n");
+    printf("3 - Imprime ABP\n");
+    printf("4 - Sair\n");
+    scanf("%d", &op);
+
+    switch (op)
+    {
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    
+}
